3abcb4f118fd4622a705d6bff65fd0933e2d46c2
Adelino Afonso <aafavelino@icloud.com>
2018-05-12 19:42:13 -0300

    Cliente-Servidor

--- src/main/java/ProjetoRedes/NetMonitor/Cliente.java
+++ src/main/java/ProjetoRedes/NetMonitor/Cliente.java
@@ -1,5 +1,22 @@
 package ProjetoRedes.NetMonitor;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Cliente {
-
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket cliente = new Socket("127.0.0.1", 12345);
+		System.out.println("O cliente se conectou ao servidor!");
+		
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		
+		while (teclado.hasNextLine()) {
+		    saida.println(teclado.nextLine());
+		}
+		
+		saida.close();
+		teclado.close();
+		cliente.close();
+}
 }
--- src/main/java/ProjetoRedes/NetMonitor/Server.java
+++ src/main/java/ProjetoRedes/NetMonitor/Server.java
@@ -1,5 +1,28 @@
 package ProjetoRedes.NetMonitor;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Server {
+	public static void main(String[] args) throws IOException{
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta!");
 
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente " +     
+            cliente.getInetAddress().getHostAddress()
+        );
+
+        Scanner s = new Scanner(cliente.getInputStream());
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());
+        }
+
+        s.close();
+        servidor.close();
+        cliente.close();
+		
+	}
+	
+	
 }
