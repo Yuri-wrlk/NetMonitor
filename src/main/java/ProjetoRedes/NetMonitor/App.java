c655cb6f4e8580a6b0a7710f80c5f6f243a6cd18
Adelino Afonso <aafavelino@icloud.com>
2018-05-13 19:23:56 -0300

    função latências pronta

--- src/main/java/ProjetoRedes/NetMonitor/App.java
+++ src/main/java/ProjetoRedes/NetMonitor/App.java
@@ -1,6 +1,7 @@
 package ProjetoRedes.NetMonitor;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 
 
@@ -10,30 +11,46 @@ import java.io.*;
  */
 public class App 
 {
-	  public static void runSystemCommand(String command) {
+	  public static  double[] runSystemCommand(String command) {
 
+		  	double [] latencias = new double [11];
 			try {
 				Process p = Runtime.getRuntime().exec(command);
 				BufferedReader inputStream = new BufferedReader(
 						new InputStreamReader(p.getInputStream()));
 
-				String s = "";
-			
-				while ((s = inputStream.readLine()) != null) {
-					System.out.println(s);
+				String s;
+				int limite = 0;
+				double n = 0;
+				
+				
+				while ((s = inputStream.readLine()) != null && limite != 10) {
+					
+					
+					 String[] tokens = s.split("time=",-1);
+					 
+					 if(tokens.length == 2) {
+						 String[] teste = tokens[1].split(" ",-1);
+						 n = Double.parseDouble(teste[0]);
+//						 System.out.println(n+"limite "+ limite);
+						 latencias[limite] = n;
+						 limite++;
+					 }	
+
 				}
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			return latencias;
 		}
 
 		public static void main(String[] args) {
 			
-			String ip = "overleaf.com";
-			runSystemCommand("ping " + ip);
-
-		
+			String ip = "ftp.jaist.ac.jp";
+			double[] y = runSystemCommand("ping " + ip);
+			
+			
 		}
 }
 
